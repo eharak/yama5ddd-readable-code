2b0a6008d7c2b0a15ab35a1436c792eaa0041c70
yamakawa <16297129+yama5ddd@users.noreply.github.com>
2019-09-24 13:48:34 +0900

    spec4: 単語の出力処理をメソッド化

--- Words/Words/Program.cs
+++ Words/Words/Program.cs
@@ -7,13 +7,22 @@ namespace Words
     {
         static void Main(string[] args)
         {
+            PutWords(args[0]);
+        }
+
+        /// <summary>
+        /// 単語をコンソールに出力する
+        /// </summary>
+        /// <param name="filepath">単語データのファイルパス</param>
+        static void PutWords(string filepath)
+        {
             Model.Words words;
-            using (var fileStream = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+            using (var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
             {
                 words = Model.Words.Load(fileStream);
             }
 
-            foreach(var word in words.WordList)
+            foreach (var word in words.WordList)
             {
                 Console.WriteLine(word.Caption);
             }
