00e1357926621a0e98e2f12d51d44426db9ee1aa
Kensuke Ehara <kensuke.ehara@justsystems.com>
2019-09-24 14:31:32 +0900

    spec5: 単語データファイルを読み込みを関数に分離

--- Words/Words/Program.cs
+++ Words/Words/Program.cs
@@ -16,15 +16,24 @@ namespace Words
         /// <param name="filepath">単語データのファイルパス</param>
         static void PutWords(string filepath)
         {
-            Model.Words words;
-            using (var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
-            {
-                words = Model.Words.Load(fileStream);
-            }
+            var words = ReadDataFile(filepath);
 
             foreach (var word in words.WordList)
             {
                 Console.WriteLine(word.Caption);
+            }
+        }
+
+        /// <summary>
+        /// 単語データファイルを読み込む
+        /// </summary>
+        /// <param name="filepath">単語データのファイルパス</param>
+        /// <returns>単語リストのデータ</returns>
+        private static Model.Words ReadDataFile(string filepath)
+        {
+            using (var fileStream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                return Model.Words.Load(fileStream);
             }
         }
     }
